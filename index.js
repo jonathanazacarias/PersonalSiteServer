b6ed1e5f42144f34959cc83e7bdc808459903af4
jonathanazacarias <40703079+jonathanazacarias@users.noreply.github.com>
2025-02-20 17:22:31 -0800

    Init commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    console.log("here")
+    res.send(`
+        <h1>Welcome</h1>
+    `);
+})
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
+  });
