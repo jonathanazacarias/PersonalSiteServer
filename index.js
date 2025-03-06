eb2f6e02dbdc6a829b88b39d104188fc1ce26260
jonathanazacarias <40703079+jonathanazacarias@users.noreply.github.com>
2025-03-05 17:45:58 -0800

    Add contact route and recaptcha verification route.

--- index.js
+++ index.js
@@ -1,17 +1,89 @@
 import express from "express";
+import cors from "cors";
+import nodemailer from "nodemailer";
+import env from "dotenv";
+import axios from "axios";
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const port = 3000;
+env.config();
 
+const GOOGLE_RECAPTCHA_SECRET_KEY = process.env.GOOGLE_RECAPTCHA_SECRET_KEY;
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+  port: 465, // true for 465, false for other ports
+  host: "smtp.gmail.com",
+  auth: {
+    user: "jonathanazacarias@gmail.com",
+    pass: process.env.GMAIL_PASS,
+  },
+  secure: true,
+});
 
 app.get("/", (req, res) => {
-    console.log("here")
-    res.send(`
+  res.send(`
         <h1>Welcome</h1>
+
     `);
+});
+
+app.post("/contact", (req, res) => {
+  const mailData = {
+    from: "jonathanazacarias@gmail.com", // sender address
+    to: "jonathanazacarias@gmail.com", // list of receivers
+    subject: `Personal Site Contact Form: ${req.body.subject}`,
+    text: "New personal site contact form message.",
+    html: `<b>Sender name: ${req.body.fName} ${req.body.lName}</b>
+           <p>${req.body.org}</p>
+           <br />
+           <p>${req.body.email}</p>
+           <br />
+           <p>${req.body.message}</p> 
+           `,
+  };
+  try {
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) {
+        console.log(err);
+        res.sendStatus(503);
+      } else {
+        // console.log(info);
+        res.send(uuidv4()).status(200);
+      }
+    });
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+app.post("/verify", async (req, res) => {
+    if(req.body.captchaToken) {
+      const captchaToken = req.body.captchaToken;
+      try {
+        const result = await axios.post(
+          `https://www.google.com/recaptcha/api/siteverify?secret=${GOOGLE_RECAPTCHA_SECRET_KEY}&response=${captchaToken}`,
+        );
+        res.send(result.data.success).status(200);
+      } catch (error) {
+        res.sendStatus(500);
+      }
+      
+    } else {
+      res.sendStatus(403);
+    }
 })
 
-
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}.`);
-  });
+  console.log(`Server listening on port ${port}.`);
+});
